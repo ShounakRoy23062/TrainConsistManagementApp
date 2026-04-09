f8419fd5f80a9001540bf541331abc7b10788933
ShounakRoy23062 <shounakofficial02@gmail.com>
2026-04-09 15:43:17 +0800

    UC10: Count Total Seats in Train (reduce)

--- App/src/TrainApp.java
+++ App/src/TrainApp.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 class Bogie {
     String name;
@@ -10,10 +8,6 @@ class Bogie {
     Bogie(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
-    }
-
-    public String toString() {
-        return name + ":" + capacity;
     }
 }
 
@@ -25,12 +19,12 @@ public class TrainApp {
 
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 54));
-        bogies.add(new Bogie("Sleeper", 70));
         bogies.add(new Bogie("First Class", 24));
 
-        Map<String, List<Bogie>> grouped = bogies.stream()
-                .collect(Collectors.groupingBy(b -> b.name));
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        System.out.println("Grouped Bogies: " + grouped);
+        System.out.println("Total Seating Capacity: " + totalSeats);
     }
 }
